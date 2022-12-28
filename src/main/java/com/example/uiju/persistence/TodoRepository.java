9e48cd4765464ba9998c577f3b2a12d515a4683b
HongUniverse <dmlwn902@naver.com>
2022-12-28 21:18:22 +0900

    테스트코드 수정

--- src/main/java/com/example/uiju/persistence/TodoRepository.java
+++ src/main/java/com/example/uiju/persistence/TodoRepository.java
@@ -4,11 +4,11 @@ import com.example.uiju.model.TodoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TodoRepository extends JpaRepository<TodoEntity, String> {
@@ -16,7 +16,6 @@ public interface TodoRepository extends JpaRepository<TodoEntity, String> {
     // ?1은 메서드의 매개변수의 순서 위치다.
     @Query("select t from TodoEntity t where t.userId = ?1")
     List<TodoEntity> findByUserId(String userId);
-
 
     @Modifying
     @Transactional
